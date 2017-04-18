8fa34265610b7632d141e432ffa9782fd7c97eb6
Albert <ayanit1@googlemail.com>
2017-04-18 18:20:34 +0100

    Add transaction feature

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,7 +4,7 @@ describe Account do
   context 'when initialised' do
     it 'balance is zero' do
       account = Account.new
-      expect(account.balance).to eq 0
+      expect(account.balance).to eql 0
     end
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,13 +1,10 @@
 require 'transaction'
 
 describe Transaction do
-  let(:deposit_amount) { 5 }
   context 'when initiated' do
-
     it 'accepts a transaction amount' do
-      allow(subject).to receive(:deposit_amount)
-      expect(transaction.amount).to eql(5)
+      withdraw = Transaction.new(5)
+      expect(withdraw.amount).to eql(5)
     end
-
   end
 end
