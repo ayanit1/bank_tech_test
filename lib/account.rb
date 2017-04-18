926c81ecf90e8fdccfc5149a4cd08eb8cc8b855b
Albert <ayanit1@googlemail.com>
2017-04-18 17:40:03 +0100

    Add create an account feature

---
+++ lib/account.rb
@@ -0,0 +1,9 @@
+class Account
+  INITIAL_BALANCE = 0
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,7 @@ require 'account'
 
 describe Account do
   context 'when initialised' do
-    it 'balance of zero' do
+    it 'balance is zero' do
       account = Account.new
       expect(account.balance).to eq 0
     end
