d07f950a08da42047e209d8ca8b01aae5c500cdd
Albert <ayanit1@googlemail.com>
2017-04-19 07:20:46 +0100

    Add spec for including date in transaction

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,11 +1,18 @@
 require 'transaction'
 
 describe Transaction do
+  subject(:transaction) { described_class.new }
   let(:deposit) { Transaction.new(5.00) }
 
   context 'when instantiated' do
     it 'accepts a deposit amount' do
       expect(deposit.amount).to equal(5.00)
+    end
+  end
+
+  describe '#date' do
+    it 'returns a date' do
+      expect(transaction.date).to be_a(DateTime)
     end
   end
 end
